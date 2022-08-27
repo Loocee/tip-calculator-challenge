e56081449e6b08aa507a5a6941256d025abc541f
Favour Ebere <102186552+Loocee@users.noreply.github.com>
2022-08-27 09:19:39 -0700

    Added files via upload

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# tip-calculator-challenge
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](/images/tipScreenshot.jpg)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Bootstrap
+- jQuery
+
+
+### What I learned
+I learned more DOM manipulations. I also tried to refactor my stylesheets. I also learned the difference between parseInt() and parseFloat().
+I believe I struggled with trying to replicate the mobile design, but I couldn't get the layout, so I did my thing. I think next time I'll try to build mobile before the system layout. 
+
+
+## Author
+<!-- - Website - [Add your name here](https://www.your-site.com) -->
+- Frontend Mentor - [@Loo_cee](https://www.frontendmentor.io/profile/Loocee)
+- Twitter - [@adaezeudele1](https://www.twitter.com/adaezeudele1?s=09)
+- LinkedIn - [Favour Ebere](https://www.linkedin.com/in/favour-ebere-79a171231)
---
+++ tip.js
@@ -0,0 +1,96 @@
+const bill = $("#bill-text");
+const button = $(".tip-buttons");
+const custom = $(".custom");
+const noOfPeople = $("#people");
+const tipPerPerson = $("#tip-amount");
+const totalPerPerson = $("#total");
+const reset = $(".reset")
+
+
+let billValue = 0;
+let percentageValue = 0;
+let percentageNumber = 0;
+let noOfPeopleValue = 0;
+let tipPerPersonValue = 0;
+let totalPerPersonValue = 0;
+
+
+var btnClicked = false;
+var customInput = false;
+
+// event listeners
+bill.on("input", function () {
+  billValue = parseFloat(bill.val());
+  // I used parseFloat() because parseInt() only retuerns a whole number while parseFloat() returns numbers that also contain decimals
+  calculator();
+  tipPerPerson.html("$0.00");
+  totalPerPerson.html("$0.00");
+});
+
+button.on("click", function () {
+  percentageValue = $(this).html();
+  btnClicked = true;
+  calculator();
+  tipPerPerson.html("$0.00");
+  totalPerPerson.html("$0.00");
+  if (btnClicked = true) {
+    if (percentageValue == "5%") {
+      percentageNumber = 5;
+    } else if (percentageValue == "10%") {
+      percentageNumber = 10;
+    } else if (percentageValue == "15%") {
+      percentageNumber = 15;
+    } else if (percentageValue == "25%") {
+      percentageNumber = 25;
+    } else if (percentageValue == "50%") {
+      percentageNumber = 50;
+    }
+  } else {
+    btnClicked = false;
+  }
+});
+
+custom.on("input", function () {
+  percentageNumber = parseFloat(custom.val());
+  calculator();
+  tipPerPerson.html("$0.00");
+  totalPerPerson.html("$0.00");
+});
+
+noOfPeople.on("input", function () {
+noOfPeopleValue = parseInt(noOfPeople.val());
+calculator();
+  if (noOfPeopleValue <= 0) {
+    $("span").css("display", "block");
+    noOfPeople.addClass("people-input");
+    tipPerPerson.html("$0.00");
+    totalPerPerson.html("$0.00");
+  } else {
+    $("span").css("display", "none");
+    noOfPeople.removeClass("people-input");
+  }
+});
+
+reset.on("click", function () {
+  tipPerPerson.html("$0.00");
+  totalPerPerson.html("$0.00");
+  if(button.hasClass("active")) {
+    button.removeClass("active");
+  }
+  percentageNumber = 0;
+  bill.val("");
+  noOfPeople.val("");
+  custom.val("");
+})
+
+
+function calculator() {
+  tipPerPersonValue = Math.round(((billValue * percentageNumber) / 100) / noOfPeopleValue);
+  totalPerPersonValue = Math.round((billValue / noOfPeopleValue) + (((billValue * percentageNumber) / 100) / noOfPeopleValue));
+  output();
+}
+
+function output() {
+  tipPerPerson.html("$" + tipPerPersonValue);
+  totalPerPerson.html("$" + totalPerPersonValue);
+}
